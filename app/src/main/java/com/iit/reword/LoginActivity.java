27fa5a66be037148bfc24de016b1dae165859ee9
Salitha <salith.2017006@iit.ac.lk>
2020-03-16 23:30:02 +0530

    signup page done

--- app/src/main/java/com/iit/reword/LoginActivity.java
+++ app/src/main/java/com/iit/reword/LoginActivity.java
@@ -2,7 +2,10 @@ package com.iit.reword;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.iit.reword.utility.Typewriter;
@@ -10,6 +13,7 @@ import com.iit.reword.utility.Typewriter;
 public class LoginActivity extends AppCompatActivity {
 
     private Typewriter txtWelcome;
+    private Button     btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,22 @@ public class LoginActivity extends AppCompatActivity {
         setupview();
     }
 
+
     private void setupview() {
 
         txtWelcome = findViewById(R.id.txtWelcome);
+        btnSignup  = findViewById(R.id.btnSignup);
+
         txtWelcome.setCharacterDelay(150);
         txtWelcome.animateText("Hello there, welcome back");
+        //txtWelcome.animateText("ආයුබෝවන්, නැවත සාදරයෙන් පිළිගනිමු");
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/iit/reword/SignupActivity.java
+++ app/src/main/java/com/iit/reword/SignupActivity.java
@@ -3,12 +3,32 @@ package com.iit.reword;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private TextInputLayout txtUsername;
+    private TextInputLayout txtPassword;
+    private TextInputLayout txtRePassword;
+
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        setupview();
+    }
+
+
+    private void setupview() {
+
+        txtUsername   = findViewById(R.id.txtUsername);
+        txtPassword   = findViewById(R.id.txtPassword);
+        txtRePassword = findViewById(R.id.txtRePassword);
+
+        username      = txtUsername.getEditText().getText().toString();
     }
 }
